26ed877ce3155d1803c5f3ee2c1df90f03672200
jack-bonetti <giacomo.bonetti@galileiostiglia.edu.it>
2025-11-25 16:55:28 +0000

    esercizio if-else

--- es1.c
+++ es1.c
@@ -3,6 +3,16 @@
 #include <stdio.h>
 
 int main(){
+    int num1, num2;
+    printf("Inserisci due numeri interi");
+    scanf("%d %d", &num1, &num2);
+
+    if(num1 > num2) {
+        printf("Il primo numero %d è maggiore del secondo %d.\n", num1, num2);
+    
+    } else {
+        printf("Il primo numero %d non è maggiore del secondo %d.\n", num1, num2);
+    }
 
     return 0;
 }
