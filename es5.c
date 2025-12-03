b607a379e53d4c962821368e08a5d7a9db06f36f
jack-bonetti <giacomo.bonetti@galileiostiglia.edu.it>
2025-12-03 08:49:52 +0000

    es.5.c temperatura

--- es5.c
+++ es5.c
@@ -1,4 +1,6 @@
-/*L’utente inserisce una temperatura in Celsius e il calcolatore la converte in Fahrenheit ed in Kelvin. Se la temperatura inserita è minore dello zero assoluto (-273,15), il calcolatore segnala un errore.
+/*L’utente inserisce una temperatura in Celsius e il calcolatore la converte
+ in Fahrenheit ed in Kelvin. Se la temperatura inserita è minore dello zero assoluto (-273,15), 
+ il calcolatore segnala un errore.
 Si ricordi che:
 Fahrenheit = (9/5) · Celsius + 32
 Kelvin = Celsius + 273,15*/
@@ -6,7 +8,18 @@ Kelvin = Celsius + 273,15*/
 #include <stdio.h>
 
 int main(){
+    float temp_celsius, temp_kelvin, temp_fahrenheit;
+    printf("inserisci la temperatura in celsius:")
+    scanf("%f", temp_celsius);
     
+    if(temp_celsius < -273,15);
+    printf("errore!"){
+    } else {
+    temp_kelvin = temp_celsius + 273,15;
+    temp fahrenheit = (9/5) * trmp_celsius + 32;
+
+    printf("la temperatura in kelvin è %f\n, temp_kelvin");
+    printf("la temperatura in fahrenheit è %f\n, temp_fahrenheit");
 
     return 0;
 }
